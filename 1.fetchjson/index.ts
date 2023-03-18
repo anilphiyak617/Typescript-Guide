8b9c02f232f20d0cc12d182f80dc2719aa4a175d
anilphiyak~ <anilphiyak617@gmail.com>
2023-03-18 09:59:07 +0530

    bug detected and fixed using typescript

--- 1.fetchjson/index.ts
+++ 1.fetchjson/index.ts
@@ -3,15 +3,26 @@ import axios from 'axios';
 const URL='https://jsonplaceholder.typicode.com/todos/1'
 
 
+interface Todo{
+   id: number;
+   title: string;
+   completed: boolean;
+}
+
+
+
 // fetch the data from the URL
 
  axios.get(URL).then((res)=>{
-   const todo = res.data;
-   const ID = todo.ID;
+   const todo = res.data as Todo;
+
+   const ID = todo.id;
    const title = todo.title;
    const completed = todo.completed;
 
-   console.log(`The todo with ID: ${ID} Has a title of: ${title},
+   console.log(`
+   The todo with ID: ${ID}
+   Has a title of: ${title},
    Is it finished? ${completed}
    `);
 
