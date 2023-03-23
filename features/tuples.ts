b6a386bbca7fd9ef2fe4221d3b4881914b17ef34
anilphiyak~ <anilphiyak617@gmail.com>
2023-03-23 09:09:06 +0530

    Tuples completed

---
+++ features/tuples.ts
@@ -0,0 +1,19 @@
+
+const drink={
+    color:'brown',
+    carbonated:true,
+    sugar:30
+};
+
+const pepsi:[string,boolean,number]=['brown',true,30];
+
+type Drink=[string,boolean,number]
+const coca:Drink=['brown',true,40]
+
+
+const carSpecs:[number,number]=[400,500]
+
+const carStats={
+    horsePower:400,
+    speed:100
+}
