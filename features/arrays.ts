87bba767ab9458936f017738224c3954edeafd2a
anilphiyak~ <anilphiyak617@gmail.com>
2023-03-23 08:58:53 +0530

    Arrays-completed

---
+++ features/arrays.ts
@@ -0,0 +1,7 @@
+// Type inference in arrays
+const carMakers=['ford','toyota','mahindra']
+const events=[new Date(),new Date(),new Date()]
+// 2d-Arrays
+// const cars: string[][]=[];
+const cars=[["suv"],["bugati",'rangerover']];
+
